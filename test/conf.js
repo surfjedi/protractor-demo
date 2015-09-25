0b542e47d29e322d79f86a9c1805eead8d5d2a38
Surfjedi <leeblazek@gmail.com>
2015-09-25 09:38:20 -0600

    updated package.json and conf for multi capabilities and phantomjs

--- test/conf.js
+++ test/conf.js
@@ -1,14 +1,49 @@
 // Tests for the calculator.
 exports.config = {
-  directConnect: true,
+  directConnect: false,
 
   framework: 'jasmine2',
 
   specs: [
     'spec.js'
   ],
-
-  capabilities: {
-    'browserName': 'chrome'
+  suites:{
+    howtos:'../howtos/**/spec.js',
+    simple: '../howtos/**/spec.js'
   },
+  multiCapabilities: [{
+    'browserName': 'phantomjs',
+    shardTestFiles: true,
+    maxInstances: 10,
+
+    /* 
+     * Can be used to specify the phantomjs binary path.
+     * This can generally be ommitted if you installed phantomjs globally.
+     */
+    'phantomjs.binary.path': require('phantomjs').path,
+
+    /*
+     * Command line args to pass to ghostdriver, phantomjs's browser driver.
+     * See https://github.com/detro/ghostdriver#faq
+     */
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+  }, {
+    browserName: 'chrome',
+      logName: 'Desktop size',
+      shardTestFiles: true,
+      maxInstances: 10,
+      'chromeOptions': {
+         args: ['--lang=en',
+           '--window-size=1500,1200']
+       }
+    },{
+      browserName: 'chrome',
+      logName: 'Mobile size',
+      shardTestFiles: true,
+      maxInstances: 10,
+      'chromeOptions': {
+         args: ['--lang=en',
+           '--window-size=414,696']
+    }
+  }]
 };
